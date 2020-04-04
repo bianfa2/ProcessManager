ad73ad301647964fefc9d676f808d122d516dd21
Fabian <fabianserna2013@gmail.com>
2020-04-04 17:54:19 -0500

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import MyProcessService from '../../services/myProcess/my-process.service';
 
 @Component({
@@ -7,9 +8,41 @@ import MyProcessService from '../../services/myProcess/my-process.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  selected: string = '0';
+  system: string = '0';
+  nProcess = new FormControl('1');
+  displayedColumns: string[] = [
+    'pid',
+    'processName',
+    'userName',
+    'memoryUsage',
+    'cpuTime',
+  ];
+  myProcess: Process[] = [
+    {
+      pid: '12',
+      processName: 'Zula.exec',
+      userName: 'bianfa',
+      memoryUsage: '12 KB',
+      cpuTime: '00:02',
+    },
+  ];
+
   constructor(public myProcessService: MyProcessService) {
     this.myProcessService.getByMemory();
   }
 
   ngOnInit(): void {}
+
+  getMyProcess() {
+    console.log(this.nProcess.value);
+  }
+}
+
+export interface Process {
+  pid: string;
+  processName: string;
+  userName: string;
+  memoryUsage: string;
+  cpuTime: string;
 }
